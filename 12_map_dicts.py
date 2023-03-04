d2812e8663003d05481bd92e07b4964a7f7d8d58
Jose Iriarte <joseiriarte2102@gmail.com>
2023-03-03 23:53:41 -0400

    Function map

--- 12_map_dicts.py
+++ 12_map_dicts.py
@@ -1,6 +1,21 @@
 items = [
     {
-      'product': 'camisa',
-      'price': 100
+        'product': 'shirt',
+        'price': 100
+    },
+    {
+        'product': 'pants',
+        'price': 250
+    },
+    {
+        'product': 'shorts',
+        'price': 30
     }
 ]
+
+prices = list(map(lambda item: item['price'], items))
+
+# new_items = [item | {'taxes': item['price'] * .19} for item in items]
+new_items = list(map(lambda item: item | {'taxes': item['price'] * .19}, items))
+print(new_items)
+print(items)
