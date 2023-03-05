0b73465630270a244aae08d72f881a59ee309a99
Jose Iriarte <joseiriarte2102@gmail.com>
2023-03-05 16:13:27 -0400

    Challenge: graphing the population of a country

--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
-import mod
+import utils
+import read_csv
+import charts
 
-keys, values = mod.get_population()
-print(keys, values)
+
+def run():
+    data = read_csv.read_csv('./app/data.csv')
+    country = input('Type Country => ')
+
+    result = utils.population_by_country(data, country)
+    print(result)
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+
+
+if __name__ == '__main__':
+    run()
--- app/mod.py
+++
@@ -1,4 +0,0 @@
-def get_population():
-    keys = ['col', 'bol', 'arg']
-    values = [300, 400, 200]
-    return keys, values
