7a6d5b43284dc6dcd151ffacd08adb7e2ba5d519
Jose Iriarte <joseiriarte2102@gmail.com>
2023-03-06 08:59:56 -0400

    Challenge: graphing the world population

--- app/main.py
+++ app/main.py
@@ -5,15 +5,24 @@ import charts
 
 def run():
     data = read_csv.read_csv('./app/data.csv')
-    country = input('Type Country => ')
 
-    result = utils.population_by_country(data, country)
-    print(result)
+    # POPULATION BY COUNTRY
+    # country = input('Type Country => ')
 
-    if len(result) > 0:
-        country = result[0]
-        labels, values = utils.get_population(country)
-        charts.generate_bar_chart(labels, values)
+    # result = utils.population_by_country(data, country)
+    # print(result)
+
+    # if len(result) > 0:
+    #     country = result[0]
+    #     labels, values = utils.get_population(country)
+    #     charts.generate_bar_chart(labels, values)
+
+
+    # WORLD POPULATION
+    data = list(filter(lambda item: item['Continent'] == 'South America', data))
+    countries = list(map(lambda item: item['Country/Territory'], data))
+    percentages = list(map(lambda item: item['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
 
 
 if __name__ == '__main__':
